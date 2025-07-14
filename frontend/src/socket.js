e92ed82d912f5ca7600ae7367036977cf77358d2
ypavanr <ypavanreddy190@gmail.com>
2025-07-15 00:59:40 +0530

    url update

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,3 +1,7 @@
 import { io } from 'socket.io-client';
-const socket = io('http://localhost:3000');
+
+const socket = io('https://sudoku-savvy.onrender.com', {
+  transports: ['websocket']
+});
+
 export default socket;
