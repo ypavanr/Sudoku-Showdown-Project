97a4151fb20483186436ea1f18bd73e8491cb932
ypavanr <ypavanreddy190@gmail.com>
2025-07-15 01:05:53 +0530

    import error patchup

--- frontend/src/components/modes/room.jsx
+++ frontend/src/components/modes/room.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import './Room.css';
+import './room.css';
 import { useNavigate } from "react-router-dom";
 import socket from "../../socket.js";
 import Logo from "../features/logo";
