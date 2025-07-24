8b56d32e242a6e1e9a3d2814ee63915489880d5f
Vibha-1802 <vibhashankar1802@gmail.com>
2025-07-24 16:12:13 +0530

    Leaderboard clean code + Leaving n Leaderboard

--- backend/src/socket.js
+++ backend/src/socket.js
@@ -1,19 +1,23 @@
 import { generateSudokuPuzzle, solve, isSamePuzzle } from "./sudoku/sudoku.js";
 import { Util } from "./sudoku/sudokuUtil.js";
 import { supabase } from "./config/supabase.js";
+
 export default function setupSocket(io){
   const roomData=new Map();
   const socketToRoom = new Map();
 
   io.on('connection',(socket)=>{
+
     socket.emit('reload')
     console.log('User connected:', socket.id);
-    socket.on( 'ready',() =>{
+
+    socket.on('ready',() =>{
        socket.emit('socket-id',socket.id);
     });
     
     socket.on('create-room',async ({roomId,mode,username,avatar,teamName})=>{
       roomData.set(roomId,{unsolvedPuzzle:null,solvedPuzzle:null,hostid:socket.id,host:username, mode: mode,players: {[socket.id]:{name:username,icon: avatar, score: 0, completed: false, team: teamName}}});
+
       if(socketToRoom.get(socket.id)){
         const prevRoom=socketToRoom.get(socket.id);
         socket.leave(prevRoom);
@@ -23,35 +27,28 @@ export default function setupSocket(io){
           roomData.delete(prevRoom);
         }
       }
+
       socketToRoom.set(socket.id,roomId);
       socket.join(roomId);
       console.log(`Room created:${roomId} by ${username}`);
       socket.emit('room-created',{roomId,mode}); 
-      const { error } = await supabase.from('game_details').insert([
-    {
-      roomid: roomId,
-      mode: mode,
-      level: null 
-    }
-  ]);
+      const { error } = await supabase.from('game_details').insert([{roomid:roomId,mode:mode,level:null }]);
 
-  if (error) console.error('Supabase insert error (game_datails):', error.message);
-  await supabase.from('username').upsert([
-  {
-    username,
-    roomid: roomId
-  }
-], { onConflict: ['username', 'roomid'] }).then(({ error }) => {
-  if (error) console.error('Supabase insert error (username):', error.message);
-});
+      if (error) console.error('Supabase insert error (game_datails):', error.message);
+      await supabase.from('username').upsert([{username,roomid: roomId}], 
+        { onConflict: ['username', 'roomid'] }).then(({ error }) => {
+          if (error) console.error('Supabase insert error (username):', error.message);
+        });
     });
 
     socket.on('join-room',async (roomId,username,avatar) => {
       const room = roomData.get(roomId);
+
       if(!room){
         socket.emit('error',"room not found");
         return;
       }
+
       if(socketToRoom.get(socket.id))
       {
         const prevRoom=socketToRoom.get(socket.id);
@@ -63,8 +60,10 @@ export default function setupSocket(io){
          roomData.delete(prevRoom);
         }
       }
+
       socketToRoom.set(socket.id,roomId);      
       socket.join(roomId);
+
       if(room.mode==='competitive'||room.mode==='cc'||room.mode==='cooperative'){
         room.players[socket.id] = { name:username, icon:avatar,score: 0, completed: false };
         io.to(roomId).emit("update-players", {
@@ -72,52 +71,48 @@ export default function setupSocket(io){
           host: room.hostid
         });
       }
+
       socket.emit('room-joined', roomId); 
       socket.emit('mode', room.mode);
       socket.to(roomId).emit('display-messages', {text:`${username} has joined the room.`,sid: "system",senderusername:username,type: "join"});
-      await supabase.from('username').upsert([
-  {
-    username,
-    roomid: roomId
-  }
-], { onConflict: ['username', 'roomid'] }).then(({ error }) => {
-  if (error) console.error('Supabase insert error (username):', error.message);
-});
+      await supabase.from('username').upsert([{username,roomid:roomId}], { 
+        onConflict: ['username', 'roomid'] }).then(({ error }) => {
+          if (error) console.error('Supabase insert error (username):', error.message);
+        });
     });
 
-socket.on('create-team', ({ roomId, teamName }) => {
-  const room = roomData.get(roomId);
-  if (!room) return;
-  room.players[socket.id].team = teamName;
-  broadcastTeams(roomId);
-});
+    socket.on('create-team', ({ roomId, teamName }) => {
+      const room = roomData.get(roomId);
+      if (!room) return;
+      room.players[socket.id].team = teamName;
+      broadcastTeams(roomId);
+    });
 
-socket.on('join-team', ({ roomId, teamName }) => {
-  const room = roomData.get(roomId);
-  if (!room) return;
-  room.players[socket.id].team = teamName;
-  broadcastTeams(roomId);
-});
+    socket.on('join-team', ({ roomId, teamName }) => {
+      const room = roomData.get(roomId);
+      if (!room) return;
+      room.players[socket.id].team = teamName;
+      broadcastTeams(roomId);
+    });
 
-socket.on('leave-team', ({ roomId }) => {
-  const room = roomData.get(roomId);
-  if (!room) return;
-  delete room.players[socket.id].team;
-  broadcastTeams(roomId);
-});
+    socket.on('leave-team', ({ roomId }) => {
+      const room = roomData.get(roomId);
+      if (!room) return;
+      delete room.players[socket.id].team;
+      broadcastTeams(roomId);
+    });
 
-function broadcastTeams(roomId) {
-  const room = roomData.get(roomId);
-  if (!room) return;
-  const teams = {};
-  for (const [id, p] of Object.entries(room.players)) {
-    const t = p.team || 'No Team';
-    teams[t] = teams[t] || [];
-    teams[t].push({ id, name: p.name, icon: p.icon });
-  }
-  io.to(roomId).emit('teams-updated', { teams });
-}
-
+    function broadcastTeams(roomId) {
+      const room = roomData.get(roomId);
+      if (!room) return;
+      const teams = {};
+      for (const [id, p] of Object.entries(room.players)) {
+        const t = p.team || 'No Team';
+        teams[t] = teams[t] || [];
+        teams[t].push({ id, name: p.name, icon: p.icon });
+      }
+      io.to(roomId).emit('teams-updated', { teams });
+    }
 
     socket.on('get-players', (roomId) => {
       const room = roomData.get(roomId);
@@ -145,6 +140,20 @@ function broadcastTeams(roomId) {
       );
     });
 
+    function broadcastleaderboard(roomId,) {
+      const room = roomData.get(roomId);
+      const allDone = Object.values(room.players).every(p => p.completed);
+      if (allDone) {
+        const leaderboard = Object.entries(room.players)
+        .map(([id, data]) => ({ playerId: id,sid: data.socketId, score: data.score, name:data.name}))
+        .sort((a, b) => b.score - a.score);
+        io.in(roomId).emit('show-leaderboard', leaderboard);
+        Object.values(room.players).forEach(player=>{
+          player.completed = false;
+        });
+      } 
+    }
+
     socket.on("leave-room", async () => {
       const roomId = socketToRoom.get(socket.id);
       if (!roomId) return;
@@ -155,11 +164,11 @@ function broadcastTeams(roomId) {
       socketToRoom.delete(socket.id);
       socket.leave(roomId);
       const clients = await io.in(roomId).allSockets();
-        if (clients.size === 0) {
-          console.log(`Room ${roomId} is now empty. Deleting room data.`);
-          roomData.delete(roomId);
-          return;
-        }
+      if (clients.size === 0) {
+        console.log(`Room ${roomId} is now empty. Deleting room data.`);
+        roomData.delete(roomId);
+        return;
+      }
       socket.to(roomId).emit("display-messages", 
         {text:`${username} has left the room.`,sid:"system",senderusername:username,type:"leave"}
       );
@@ -178,9 +187,9 @@ function broadcastTeams(roomId) {
         }  
         else {roomData.delete(roomId);}
       }
-       io.to(roomId).emit("update-players",{players: room.players,
-        host: room.hostid
-       });
+
+      io.to(roomId).emit("update-players",{players: room.players,host: room.hostid});
+      broadcastleaderboard(roomId);
     });
 
     socket.on("duration-change", ({ roomId, duration }) => {
@@ -190,6 +199,7 @@ function broadcastTeams(roomId) {
     socket.on("difficulty-change",({roomId,difficulty})=>{
     socket.to(roomId).emit("update-difficulty", difficulty);
     })
+
     socket.on("validation-change",({roomId,validation})=>{
     socket.to(roomId).emit("update-validation", validation);
     })
@@ -204,8 +214,8 @@ function broadcastTeams(roomId) {
         socket.emit('not-host');
         return;
       }
-       socket.to(roomId).emit("update-difficulty", difficulty);
-       socket.to(roomId).emit("update-validation", validation);
+      socket.to(roomId).emit("update-difficulty", difficulty);
+      socket.to(roomId).emit("update-validation", validation);
       const unsolvedPuzzle = generateSudokuPuzzle(difficulty);
       const solvedPuzzle = JSON.parse(JSON.stringify(unsolvedPuzzle)); 
       solve(solvedPuzzle); 
@@ -215,12 +225,12 @@ function broadcastTeams(roomId) {
       console.log(`Game started in room ${roomId}.difficulty level: ${difficulty} solved puzzle:`);
       Util.print2DArray(solvedPuzzle);
       await supabase
-  .from('game_details')
-  .update({ level: difficulty })
-  .eq('roomid', roomId)
-  .then(({ error }) => {
-    if (error) console.error('Supabase update error (game_datails.level):', error.message);
-  });
+      .from('game_details')
+      .update({ level: difficulty })
+      .eq('roomid', roomId)
+      .then(({ error }) => {
+        if (error) console.error('Supabase update error (game_datails.level):', error.message);
+      });
     });
 
     socket.on('check-host',(roomId)=>{
@@ -276,36 +286,25 @@ function broadcastTeams(roomId) {
       name = room.players[socket.id].name;
       socket.emit('new-points',points);
       socket.emit('game-complete', "Puzzle solved! Hooray!!!");
-      io.in(roomId).emit('player-finished', {
-        playerId: socket.id,
-        points,
-        name,
-      });
-   
+      io.in(roomId).emit('player-finished', {playerId:socket.id,points,name,});
+      broadcastleaderboard(roomId);
+    }
+    else {
+      socket.emit('game-incomplete', 'Game not yet completed');
+    }
+  });
 
-      const allDone = Object.values(room.players).every(p => p.completed);
-      if (allDone) {
-        const leaderboard = Object.entries(room.players)
-        .map(([id, data]) => ({ playerId: id,sid: data.socketId, score: data.score, name:data.name}))
-        .sort((a, b) => b.score - a.score);
-        io.in(roomId).emit('show-leaderboard', leaderboard);
-      }} 
-      else {
-        socket.emit('game-incomplete', 'Game not yet completed');
-      }
+    socket.on("expert-input", ({ roomId, row, col, number }) => {
+      socket.to(roomId).emit("update-expert-input", { row, col, number });
     });
-   socket.on("expert-input", ({ roomId, row, col, number }) => {
-  socket.to(roomId).emit("update-expert-input", { row, col, number });
-});
 
-socket.on("expert-clear", ({ roomId, row, col }) => {
-  socket.to(roomId).emit("update-expert-clear", { row, col });
-});
+    socket.on("expert-clear", ({ roomId, row, col }) => {
+      socket.to(roomId).emit("update-expert-clear", { row, col });
+    });
  
-
     socket.on('expert-submission-competitive',({roomId,remaining})=>{
       const roomID = socketToRoom.get(socket.id);
-       if (!roomID) return;
+      if (!roomID) return;
       const room = roomData.get(roomId);
       let name;
       if (!room.players[socket.id]) return;
@@ -313,34 +312,23 @@ socket.on("expert-clear", ({ roomId, row, col }) => {
       room.players[socket.id].completed = true;
       room.players[socket.id].score = points;
       name = room.players[socket.id].name;
-      io.in(roomId).emit('player-finished', {
-        playerId: socket.id,
-        points,
-        name,
-      });
-      const allDone = Object.values(room.players).every(p => p.completed);
-      if (allDone) {
-        const leaderboard = Object.entries(room.players)
-        .map(([id, data]) => ({ playerId: id,sid: data.socketId, score: data.score, name:data.name}))
-        .sort((a, b) => b.score - a.score);
-        io.in(roomId).emit('show-leaderboard', leaderboard);
-      }
+      io.in(roomId).emit('player-finished', {playerId: socket.id,points,name,});
+      broadcastleaderboard(roomId);
     })
-   socket.on("expert-submission-cooperative",  (roomId)=>{
-        io.in(roomId).emit('game-complete',"Puzzle solved! Hooray!!!");
-       } );
+
+    socket.on("expert-submission-cooperative",  (roomId)=>{
+      io.in(roomId).emit('game-complete',"Puzzle solved! Hooray!!!");
+    });
+
     socket.on("time-up", (roomId,points) => {
       const room = roomData.get(roomId);
-      
       if (!room) return;
       const player = room.players[socket.id];
-       if (!player.completed) {
-    player.score = points;
-  }
-      const leaderboard = Object.entries(room.players)
-      .map(([id, data]) => ({ playerId: id,score: data.score,name:data.name }))
-      .sort((a, b) => b.score - a.score);
-      io.in(roomId).emit("show-leaderboard", leaderboard);
+      if (!player.completed) {
+        player.score = points;
+        player.completed = true;
+      }
+      broadcastleaderboard(roomId);
     });
 
     socket.on("clear-cell",({roomId,row,col})=>{ 
@@ -350,7 +338,7 @@ socket.on("expert-clear", ({ roomId, row, col }) => {
     });
 
     socket.on('disconnect', async () => {
-        const roomId = socketToRoom.get(socket.id);
+      const roomId = socketToRoom.get(socket.id);
       if (!roomId) return;
       const room = roomData.get(roomId);
       if (!room) return;
@@ -359,11 +347,11 @@ socket.on("expert-clear", ({ roomId, row, col }) => {
       socketToRoom.delete(socket.id);
       socket.leave(roomId);
       const clients = await io.in(roomId).allSockets();
-        if (clients.size === 0) {
-          console.log(`Room ${roomId} is now empty. Deleting room data.`);
-          roomData.delete(roomId);
-          return;
-        }
+      if (clients.size === 0) {
+        console.log(`Room ${roomId} is now empty. Deleting room data.`);
+        roomData.delete(roomId);
+        return;
+      }
       socket.to(roomId).emit("display-messages", 
         {text:`${username} has left the room.`,sid:"system",senderusername:username,type:"leave"}
       );
@@ -384,10 +372,8 @@ socket.on("expert-clear", ({ roomId, row, col }) => {
            console.log(`Room ${roomId} is now empty. Deleting room data.`);
         }
       }
-       io.to(roomId).emit("update-players",{players: room.players,
-        host: room.hostid
-       });
+      io.to(roomId).emit("update-players",{players: room.players,host: room.hostid});
+      broadcastleaderboard(roomId);
     });
-    
   });
 }
